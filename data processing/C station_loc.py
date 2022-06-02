5d94e2fa34042e01dc053475d2e3d0902c3c9882
KP-Zhang <Dylan-cheung@hotmail.com>
2022-06-02 16:19:35 +0800

    Update C station_loc.py

--- data processing/C station_loc.py
+++ data processing/C station_loc.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-location_path = 'G:/Data/PeMS/2013/District 7/station_location/'
+location_path = '/PeMS/2013/District 7/station_location/'
 station_df = pd.read_csv(location_path + 'd07_text_meta_2013_11_07.txt', sep='\t')
 print(station_df['Type'].describe())
 
@@ -38,31 +38,3 @@ for type_i in np.unique(type_lst):
 print(len(id_lst))
 print(len(type_lst))
 print(np.unique(type_lst))
-exit()
-
-file_path_save = 'G:/Data/PeMS/2013/District 7/csv_2022/'
-file_lst = []
-ids_lst = []
-for root, dirs, files in os.walk(file_path_save):
-    for file_name in files:
-        if file_name.endswith('.csv'):
-            file_lst.append(file_name)
-
-print('Number of files', len(file_lst))
-for file_i in file_lst[360:361]:
-    print(file_i)
-    station_df = pd.read_csv(file_path_save + file_i,
-                             dtype={'Station_ID': int, 'Lane_Type': str,
-                                    'Flow': int, 'Occupancy': float}, engine='c')
-    # print(station_df['Station_ID'].unique())
-    print(len(station_df['Station_ID'].unique()))
-    ids_lst.append(station_df['Station_ID'].unique())
-
-    for id_i in sel_id_arr:
-        temp_df = station_df.loc[station_df['Station_ID'] == id_i]
-        print('temp_df.shape', temp_df.shape)
-        plt.plot(temp_df['Flow'])
-        plt.title(str(id_i))
-        plt.tight_layout()
-        plt.show()
-        # exit()
